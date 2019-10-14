1969a59680a99c50d8be33133ad362d612fb0189
FAHRI KAAN GOKTUNA <TCFGOKTUNA@turkcell.entp.tgc>
2019-10-14 23:27:54 +0300

    initial commit

---
+++ src/EFCoreTools/SampleEntity/AnEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreTools.SampleEntity
+{
+    public class AnEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
